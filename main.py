be1712424c47232ba62fa0925635b09b69ce25de
Joshua Lemmon <joshua.lemmon@uoit.net>
2019-03-21 13:45:10 -0400

    Created db to store results in

--- main.py
+++ main.py
@@ -57,13 +57,13 @@ def main():
 	IMAGE_DIR = "./images"
 	file_names = next(os.walk(IMAGE_DIR))[2]
 	image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-
+	for c in class_names:
+		print(c)
 	# Run detection
 	results = model.detect([image], verbose=1)
 
 	# Visualize results
 	r = results[0]
-	print("dasdasd")
 	print(r)
 	visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
 	                            class_names, r['scores'])
---
+++ populate_db.py
@@ -0,0 +1,44 @@
+import sqlite3 
+import os
+import argparse
+
+
+def add_images(db, img_dir):
+	c = db.cursor()
+	for i,f in enumerate(os.listdir(img_dir)):
+		c.execute("insert into files(file_id, filename) values ("+str(i)+", '" + f + "');")
+	db.commit()
+		
+def add_videos(db, vid_dir):
+	c = db.cursor()
+	for i,f in enumerate(os.listdir(vid_dir)):
+		c.execute("insert into files(file_id, filename) values ("+str(i)+", '" + f + "');")
+	db.commit()
+
+def add_labels(db):
+	labels = []
+	with open('labels.txt', 'r') as f:
+		for l in f.readlines():
+			labels.append(l.strip('\n'))
+	c = db.cursor()
+	for i, l in enumerate(labels):
+		c.execute("insert into labels(label_id, label) values ("+str(i) + ", '" + l +"');")
+	db.commit()
+
+def main(args):
+	db = sqlite3.connect(args.db_path)
+	c = db.cursor()
+	c.execute("delete from files;")
+	c.execute("delete from labels;")
+	db.commit()
+	add_labels(db)
+	add_images(db, args.img_dir)
+	add_videos(db, args.vid_dir)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-db', '--db_path', type=str, default='labeldb.db')
+	parser.add_argument('-id', '--img_dir', type=str, default='./images')
+	parser.add_argument('-vd', '--vid_dir', type=str, default='./videos')
+	main(parser.parse_args())
